305948f5d26e7d8f5eb3ef84b491db8a0ed4b244
Bartłomiej Zimny <bartekziimny90@gmail.com>
2018-12-16 21:47:14 +0100

    poprawa logowania

--- routes/auth/loginUser.js
+++ routes/auth/loginUser.js
@@ -5,6 +5,8 @@ const status = require("../../status");
 
 const crypto = require("crypto-js");
 
+const { LocalDateTime, nativeJs } = require("js-joda");
+
 const sql = `UPDATE public."user"
 set "accessToken" = $3 , "accessTokenExpiresAt" = NOW()+ INTERVAL '1 day', "refreshToken" = $4, "refreshTokenExpiresAt" = NOW() + INTERVAL '1 week'
 where "login" = $1 AND "password" = $2 AND "accessTokenExpiresAt" < NOW() OR "refreshTokenExpiresAt" < NOW() OR "accessTokenExpiresAt" IS NULL OR "refreshTokenExpiresAt" IS NULL;
@@ -12,6 +14,10 @@ where "login" = $1 AND "password" = $2 AND "accessTokenExpiresAt" < NOW() OR "re
 SELECT "accessToken", "accessTokenExpiresAt", "refreshToken" , "refreshTokenExpiresAt" 
 from "user"
 where "login" = $1`;
+
+const sql1 = `SELECT "accessToken", "accessTokenExpiresAt", "refreshToken" , "refreshTokenExpiresAt" 
+from "user"
+where "login" = $1 AND "password" = $2`;
 
 router.post("/", function (req, res) {
   var user = req.body.username;
@@ -24,16 +30,24 @@ router.post("/", function (req, res) {
   var refreshToken = crypto
     .SHA256(user + new Date().toString())
     .toString(crypto.enc.Hex);
-    console.log("password "+pass);
-    console.log("accessToken "+accessToken);
-    console.log("refreshToken "+refreshToken);
 
   db
-    .query(sql, [user, pass, accessToken, refreshToken])
+    .query(sql1, [user, pass])
     .then(response => {
-        res.json({ data: response, status: status.OK.code, message: status.OK.message });
+      if (response.rowCount == 0 || response.rowCount > 1) {
+        res.json({status: status.DeniedLogin.code, message: status.DeniedLogin.message});
+      } else {
+        const data = response.rows[0];
+        const {accessToken, accessTokenExpiresAt, refreshToken, refreshTokenExpiresAt} = data;
+        const now = LocalDateTime.now();
+        const accessTokenDate = LocalDateTime.from(nativeJs(new Date(accessTokenExpiresAt)));
+        const refreshTokenDate = LocalDateTime.from(nativeJs(new Date(refreshTokenExpiresAt)));
+        if (accessToken && refreshToken &&  refreshTokenDate.isAfter(now))
+          res.json({data, status: status.OK.code, message: status.OK.message});
+        }
+      
     })
-    .catch(error => res.json({ status: status.Error.code, message: error }));
+    .catch(error => res.json({status: status.Error.code, message: error}));
 
 });
 
